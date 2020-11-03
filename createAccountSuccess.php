00b280e1846d28aece0731163175d21a52ef7367
arielhaberman <46663812+arielhaberman@users.noreply.github.com>
2020-11-03 00:42:45 -0600

    Create createAccountSuccess.php

---
+++ createAccountSuccess.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html> 
+<html lang="en">
+<head>
+    <title>Account Created</title>
+    <link rel="stylesheet" href="stylesheet.css">
+</head>
+<body>
+    <h2>Success!</h2><br> 
+    <p1>Your account was successfully created.</p1>
+    <br>
+    <br>
+    <a href="http://ec2-3-135-228-70.us-east-2.compute.amazonaws.com/~arielhaberman/module5-test/login.php">Go back to login page</a>
+</body>
+</html>
